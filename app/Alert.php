9eca6dbffd16a77b9cc6a7a5a3c47f36549f6d79
Javier Vidal <javier.vidal@sngular.team>
2017-10-02 16:04:00 +0200

    Add alert model, migration and relationships

---
+++ app/Alert.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'notified_at',
+        'checked_at',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the alerts for the account.
+     */
+    public function alerts()
+    {
+        return $this->hasMany('App\Alert');
+    }
 }
