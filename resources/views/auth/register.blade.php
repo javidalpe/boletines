840c942ecbf626e1ae9518ccdce0305b193cd4c7
Javier Vidal <javier.vidal@sngular.team>
2017-10-09 19:14:15 +0200

    Added copy to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,12 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <p>Para poder convertir una búsqueda en alerta diaria necesitamos que te registres para poder gestionar esta
+                u otras alertas que crees y que nos des un mail donde mandarte las alertas.
+
+                Como explicamos en <a href="{{route('about')}}">Acerca de nosotros</a> estamos en una fase de
+                investigación en este proyecto y solo vamos a usarlos para este fin. Podrás darte de baja y todos tus
+                datos serán eliminados.</p>
             <div class="panel panel-default">
                 <div class="panel-heading">Crea una cuenta nueva</div>
 
@@ -14,7 +20,8 @@
                             <label for="name" class="col-md-4 control-label">Nombre de la persona de contacto</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
+                                       required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -28,7 +35,8 @@
                             <label for="email" class="col-md-4 control-label">Dirección de correo electrónico</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -56,7 +64,8 @@
                             <label for="password-confirm" class="col-md-4 control-label">Repite la contraseña</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control"
+                                       name="password_confirmation" required>
                             </div>
                         </div>
 
