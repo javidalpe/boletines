8980d60720e7b7d44dd13d64661c6ff1adc8ce44
Javier Vidal <javier.vidal@sngular.team>
2017-10-10 13:04:19 +0200

    Fixed auth controllers redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/search';
+    protected $redirectTo = '/alerts';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/search';
+    protected $redirectTo = '/alerts';
 
     /**
      * Create a new controller instance.
