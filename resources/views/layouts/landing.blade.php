0da8189aab06942097f6873d0bf31f21f01fcd43
Javier Vidal <javier.vidal@sngular.team>
2017-10-11 15:32:31 +0200

    Removed google suit verification code

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -3,7 +3,6 @@
 <head>
     @include('layouts.meta')
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <meta name="google-site-verification" content="gUmuzUFcJ_5nbSlsYKiBLR_hVBe1h4htYqlUDAKRy4E" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('styles')
