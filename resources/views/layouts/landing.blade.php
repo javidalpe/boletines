a0188ae55606f8105e0465c5c335b6fb9ef07c96
Javier Vidal <javier.vidal@sngular.team>
2017-10-11 15:09:51 +0200

    Added meta to google verification process

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     @include('layouts.meta')
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta name="google-site-verification" content="gUmuzUFcJ_5nbSlsYKiBLR_hVBe1h4htYqlUDAKRy4E" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('styles')
