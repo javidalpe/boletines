ca46f858de09ff8b1a9aed84abd85c11a58e030d
Javier Vidal <javier.vidal@sngular.team>
2017-10-11 16:03:27 +0200

    Added password reset notification

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\UserRegistered' => [
             'App\Listeners\UserRegisteredEventListener',
         ],
+        'Illuminate\Auth\Events\PasswordReset' => [
+            'App\Listeners\PasswordResetEventListener',
+        ]
     ];
 
     /**
