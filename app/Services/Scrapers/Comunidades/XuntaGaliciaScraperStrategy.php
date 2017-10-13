630223a3a8a636e1dcae9273ed2dde1b706ea2e9
Javier Vidal <javier.vidal@sngular.team>
2017-10-13 12:52:46 +0200

    Fixed Galicia scraper to download spanish documents

--- app/Services/Scrapers/Comunidades/XuntaGaliciaScraperStrategy.php
+++ app/Services/Scrapers/Comunidades/XuntaGaliciaScraperStrategy.php
@@ -15,7 +15,7 @@ class XuntaGaliciaScraperStrategy implements IBoletinScraperStrategy
 
 	public function downloadFilesFromInternet()
 	{
-		return FileDownloaderScraper::create("http://www.xunta.es/doga/index.htm")
+		return FileDownloaderScraper::create("http://www.xunta.es/doga/index.htm?lang=es")
 			->forEachLink ("/mostrarContenido\.do\?paginaCompleta=false&idEstado=\d+&rutaRelativa=true&ruta=\/\d+\/\d+\/Secciones\w+\.html/", self::MAX_NUMBER_OF_PUBLICATIONS)
 			->navigate()
 			->getLinks("/\/dog\/Publicados\/\d+\/\d+\/\w+-\w+-\w+\.pdf/");
