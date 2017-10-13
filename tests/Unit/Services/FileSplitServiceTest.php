195222c877d1931517122cc81bf4f8049ee796bc
Javier Vidal <javier.vidal@sngular.team>
2017-10-13 13:03:35 +0200

    Fixed Split Service to do not split words

--- tests/Unit/Services/FileSplitServiceTest.php
+++ tests/Unit/Services/FileSplitServiceTest.php
@@ -19,11 +19,23 @@ class FileSplitServiceTest extends TestCase
 		$this->assertEquals(["1234", "1234"], $service->splitDocument("123412341"));
 		$this->assertEquals(["1234", "1234", "12"], $service->splitDocument("1234123412"));
 
-		$this->assertEquals(["12 3", "4123"], $service->splitDocument("12\n341234"));
-		$this->assertEquals(["12 3", "4123"], $service->splitDocument("12\n\n341234"));
-		$this->assertEquals(["12 3", "4123", "45"], $service->splitDocument("\n 12\n\n3412345"));
+		$this->assertEquals(["12", "3412"], $service->splitDocument("12\n34123"));
+		$this->assertEquals(["12", "3412", "34"], $service->splitDocument("12\n\n341234"));
+		$this->assertEquals(["12", "3412", "345"], $service->splitDocument("\n 12\n\n3412345"));
 
-		$this->assertEquals(["123", "41 2", "3412", "34"], $service->splitDocument("123\n41\n2341234"));
+		$this->assertEquals(["123", "41", "2341", "234"], $service->splitDocument("123\n41\n2341234"));
+
+        $this->assertEquals(["12", "3412"], $service->splitDocument("12 34123"));
+        $this->assertEquals(["12", "3412", "34"], $service->splitDocument("12  341234"));
+        $this->assertEquals(["12", "3412", "345"], $service->splitDocument("  12  3412345"));
+
+        $this->assertEquals(["123", "41", "2341", "234"], $service->splitDocument("123 41 2341234"));
+
+        $this->assertEquals(["12", "3412"], $service->splitDocument("12\n 34123"));
+        $this->assertEquals(["12", "3412", "34"], $service->splitDocument("12 \n\n 341234"));
+        $this->assertEquals(["12", "3412", "345"], $service->splitDocument("  12  \n3412345"));
+
+        $this->assertEquals(["123", "41", "2341", "234"], $service->splitDocument("123\n 41 \n2341234"));
 
 	}
 }
