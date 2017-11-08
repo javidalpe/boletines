11348059d6553c3cd7894b9550cfa5e8c46f4f09
Javier Vidal <javier.vidal@sngular.team>
2017-11-08 15:54:38 +0100

    Fix google analytics sdk url

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,6 +1,6 @@
 @if (config('services.google-analytics.id'))
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109372316-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google-analytics.id') }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag() {
