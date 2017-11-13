b364b30acb06022be7b8a4e8d1ecd45a48b8d639
Javier Vidal <javier.vidal@sngular.team>
2017-11-13 10:00:12 +0100

    Update alert to IE 11

--- resources/views/shared/search.blade.php
+++ resources/views/shared/search.blade.php
@@ -1,4 +1,4 @@
-<!--[if lte IE 9]>
+<!--[if lte IE 11]>
 <div class="col-md-6 col-md-offset-3">
     <div class="alert alert-warning">Atención, el buscador de {{config('app.name')}} no funciona en esta versión de Internet Explorer. Te recomendamos que utilices la última versión de tu navegador o que descargues el navegador Google Chrome o Mozilla Firefox.</div>
 </div>
