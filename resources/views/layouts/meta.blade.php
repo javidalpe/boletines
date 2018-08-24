bfd074e4fb0111f8e80bcf78504dc4b3b92eee1b
Javier Vidal <jvpenya@gmail.com>
2018-08-24 23:41:04 +0200

    Add theme color meta tag

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -2,3 +2,4 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#008a8f"/>
