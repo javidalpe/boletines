d7c91926aa009a90903936de54adda30238044b5
Javier Vidal <jvpenya@gmail.com>
2019-02-04 21:44:07 +0100

    Update theme color. Avoid autozoom on ios safari.

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,6 +1,6 @@
 @include('layouts.tag-manager')
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="theme-color" content="#008a8f"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="theme-color" content="#034ea1"/>
 <link rel="manifest" href="/manifest.json">
