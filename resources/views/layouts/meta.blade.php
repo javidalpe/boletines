000cce7fd84d99ee4fcac039049ef17835befa48
Javier Vidal <javier.vidal@sngular.team>
2019-02-12 17:02:32 +0100

    Fix meta and manifest

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -2,5 +2,14 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta name="theme-color" content="#034ea1"/>
+
+<meta name="msapplication-TileColor" content="#034ea1">
+<meta name="theme-color" content="#034ea1">
+
 <link rel="manifest" href="/manifest.json">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+
