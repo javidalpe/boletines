a1faf4ccf0ec8b8c60bc954b7edb3bd29b7d24da
Javier Vidal <jvpenya@gmail.com>
2019-03-30 14:15:16 +0100

    Fix Valladolid scraper

--- app/Services/Scrapers/ScraperStrategy/Provinces/ValladolidScraperStrategy.php
+++ app/Services/Scrapers/ScraperStrategy/Provinces/ValladolidScraperStrategy.php
@@ -4,13 +4,16 @@ namespace App\Services\Scrapers\Europeo;
 
 use App\Services\Scrapers\HTMLScraper;
 use App\Services\Scrapers\IBoletinScraperStrategy;
+use Carbon\Carbon;
 
 class ValladolidScraperStrategy implements IBoletinScraperStrategy
 {
 
     public function downloadFilesFromInternet()
     {
+    	$now = Carbon::now();
+    	$month = $now->formatLocalized('%B');
         return HTMLScraper::create("https://bop.sede.diputaciondevalladolid.es/?bop=ultimo")
-            ->getLinks("/\/boletines\/\d+\/febrero\/\d+\/BOPVA\-A\-\d+\-\d+\.pdf/");
+            ->getLinks("/\/boletines\/\d+\/$month\/\d+\/BOPVA\-B\-\d+\-\d+\.pdf/");
     }
 }
