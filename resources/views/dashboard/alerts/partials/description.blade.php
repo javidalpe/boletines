42f717510eec8ca981984f816811c2750b7c1afb
javidalpe <javier@undefinedlabs.com>
2019-07-27 12:06:47 +0200

    Update price copies

--- resources/views/dashboard/alerts/partials/description.blade.php
+++ resources/views/dashboard/alerts/partials/description.blade.php
@@ -1,3 +1,2 @@
-Las alertas tienen un coste de 15€/mes por alerta las 5 primeras alertas, 12€/mes por alerta hasta 20 alertas
-y 8€/mes por alerta para más de 20 alertas. IVA no incluido. Si necesitas un volumen mayor de alertas,
+Las alertas tienen un coste de 5€/mes por alerta. Si necesitas un volumen grande de alertas,
 <a href="{{ route('contact') }}"> contacta con nosotros</a>.
--- resources/views/dashboard/alerts/partials/price.blade.php
+++ resources/views/dashboard/alerts/partials/price.blade.php
@@ -1,19 +1,7 @@
-@php
-    $count = $alertsCount;
-@endphp
-
-
 <div class="form-group">
     <label for="card-element">
         Precio de la alerta:
     </label>
-        @if ($count < 5)
-            <h2>15€<small>/mes</small></h2>
-        @elseif ($count < 20)
-            <h2>12€<small>/mes</small></h2>
-        @else
-            <h2>8€<small>/mes</small></h2>
-        @endif
-    <p class="help-block">@include('dashboard.alerts.partials.description')</p>
-    </p>
+    <h2>5€<small>/mes</small></h2>
+    <p class="help-block">IVA no incluido</p>
 </div>
