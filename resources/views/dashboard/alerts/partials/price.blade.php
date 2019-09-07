fa64dd598e5461823dd03ad34bfc5bb9f4d362f2
javidalpe <javier@undefinedlabs.com>
2019-09-07 12:37:08 +0200

    Revert "Update price copies"
    This reverts commit 42f717510eec8ca981984f816811c2750b7c1afb.

--- resources/views/dashboard/alerts/partials/price.blade.php
+++ resources/views/dashboard/alerts/partials/price.blade.php
@@ -1,7 +1,19 @@
+@php
+    $count = $alertsCount;
+@endphp
+
+
 <div class="form-group">
     <label for="card-element">
         Precio de la alerta:
     </label>
-    <h2>5€<small>/mes</small></h2>
-    <p class="help-block">IVA no incluido</p>
+        @if ($count < 5)
+            <h2>15€<small>/mes</small></h2>
+        @elseif ($count < 20)
+            <h2>12€<small>/mes</small></h2>
+        @else
+            <h2>8€<small>/mes</small></h2>
+        @endif
+    <p class="help-block">@include('dashboard.alerts.partials.description')</p>
+    </p>
 </div>
