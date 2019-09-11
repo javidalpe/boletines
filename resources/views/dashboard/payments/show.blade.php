65d840e9951c006b623e9a27f70d8b6fec347d52
Javier Vidal <jvpenya@gmail.com>
2019-09-11 23:42:17 +0200

    Fix default card

--- resources/views/dashboard/payments/show.blade.php
+++ resources/views/dashboard/payments/show.blade.php
@@ -2,9 +2,9 @@
 $card = Auth::user()->defaultPaymentMethod()->card;
 @endphp
 
-<div class="panel panel-default panel-body">
-Tarjeta: {{$card->brand}} ••••{{ $card->last4 }} - Fecha de expiración: ({{
-$card->exp_month }}/{{
-$card->exp_year
- }})
-</div>
+@isset($card)
+	<div class="panel panel-default panel-body">
+		Tarjeta: {{$card->brand}} ••••{{ $card->last4 }} - Fecha de expiración: ({{
+		$card->exp_month }}/{{$card->exp_year}})
+	</div>
+@endisset
