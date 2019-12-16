9d31066086c73becf15c80213ab64ce40c433a5d
javidalpe <javier@undefinedlabs.com>
2019-12-16 10:03:45 +0100

    Preconnect, lazy loading

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -43,15 +43,15 @@
     </div>
     <div class="row" style="margin-top: 40px; margin-bottom: 40px">
         <div class="col-md-2">
-            <img class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+            <img loading="lazy" class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
                  data-src="{{mix('img/boe.webp')}}" alt="Busca en el Boletín Oficial del Estado">
         </div>
         <div class="col-md-4">
-            <img class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+            <img loading="lazy" class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
                  data-src="{{mix('img/lex.webp')}}" alt="Busca en el Diario Oficial de la Unión Europea">
         </div>
         <div class="col-md-2 col-md-offset-3">
-            <img class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="{{mix('img/powered_by_stripe@3x.png')}}" alt="Con la tecnología de Stripe">
+            <img loading="lazy" class="img-responsive" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="{{mix('img/powered_by_stripe@3x.png')}}" alt="Con la tecnología de Stripe">
         </div>
     </div>
 </div>
--- resources/views/layouts/preconnect.blade.php
+++ resources/views/layouts/preconnect.blade.php
@@ -1,1 +1,2 @@
 <link rel="preconnect" href="https://www.google-analytics.com">
+<link rel="preconnect" href="https://storage.googleapis.com">
