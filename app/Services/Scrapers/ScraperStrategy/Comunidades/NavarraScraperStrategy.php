b007974aa5960c64f4ed788e156dd40dd0ab6fce
Javier Vidal <jvpenya@gmail.com>
2020-03-03 15:06:48 -0800

    Fix Navarra Scraper

--- app/Services/Scrapers/ScraperStrategy/Comunidades/NavarraScraperStrategy.php
+++ app/Services/Scrapers/ScraperStrategy/Comunidades/NavarraScraperStrategy.php
@@ -15,8 +15,8 @@ class NavarraScraperStrategy implements IBoletinScraperStrategy
 
 	public function downloadFilesFromInternet()
 	{
-		return HTMLScraper::create("http://www.navarra.es/home_es/Actualidad/BON/")
-			->getLinks ("/\/home_es\/Actualidad\/BON\/Boletines\/\d+\/\d+\/boletin.pdf/");
+		return HTMLScraper::create("https://bon.navarra.es/es/inicio")
+			->getLinks ("/https\:\/\/bon\.navarra\.es\/es\/inicio\?p_p_id\=es_navarra_bon_detalle_portlet_boletin_DetalleBoletinPortlet&p_p_lifecycle\=\d+&p_p_state\=normal&p_p_mode\=view&p_p_resource_id\=descargarArchivo&p_p_cacheability\=cacheLevelPage&_es_navarra_bon_detalle_portlet_boletin_DetalleBoletinPortlet_numero\=\d+&_es_navarra_bon_detalle_portlet_boletin_DetalleBoletinPortlet_idioma\=es_ES&_es_navarra_bon_detalle_portlet_boletin_DetalleBoletinPortlet_anyo\=\d+/");
 	}
 
 
